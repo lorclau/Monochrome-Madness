e3d8cfef4cd215361c6ce98e8afe3e0c694cb481
lorclau <167276218+lorclau@users.noreply.github.com>
2024-06-12 23:36:51 -0700

    Add files via upload

---
+++ src/Scenes/Credits.js
@@ -0,0 +1,85 @@
+class Credits extends Phaser.Scene {
+    constructor() {
+        super("platformerCreditsScene");
+    }
+
+    preload() {
+        //load assets
+        this.load.setPath("./assets/");
+        this.load.image("title_text", "MADNESS.png");
+        this.load.image("gem1", "gem_001.png");
+    }
+
+    create() {
+        //send message to console
+        console.log("Credit screen.");
+
+        //key for player input
+        this.spaceKey = this.input.keyboard.addKey('SPACE');
+
+        //change background color
+        var div = document.getElementById('phaser-game');
+        div.style.backgroundColor = "#292929";
+
+        // display loading screen
+        this.title = this.add.image( 1150, 150, "title_text").setScale(0.45);
+        const gem = this.add.image(750, 600, "gem1").setScale(5).setOrigin(0.5,0.15);
+
+        this.tweens.add({
+            targets: this.title,
+            duration: 2000,
+            scaleX: 0.5,
+            scaleY: 0.5,
+            ease: 'bounce.out',
+            repeat: 0,
+            yoyo: false,
+            onComplete: () => this.promptPlayer(gem)
+        });
+
+        this.add.text(200, 220, 'Created by\nLORRAINE TORRES\nfor CMPM 120, UCSC',
+            { 
+                fontFamily: 'Times, serif'
+            }).setScale(0.2).setResolution(10);
+    
+            this.add.text(200, 430, 'Graphics and Assets provided by\nKENNEY at www.kenney.nl',
+                { 
+                    fontFamily: 'Times, serif'
+                }).setScale(0.2).setResolution(10);
+
+    }
+
+    update() {
+        if(Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
+            //start first level
+            this.scene.start("platformerTitleScene");
+        }
+    }
+
+    //functions
+    promptPlayer(gem)
+    {
+        const chain1 = this.tweens.chain({
+            targets: gem,
+            tweens: [
+                {
+                    y: 470,
+                    scaleX: 3,
+                    duration: 300,
+                    ease: 'quad.out'
+                },
+                {
+                    y: 600,
+                    scaleX: 5,
+                    duration: 1000,
+                    ease: 'bounce.out'
+                },
+            ],
+            loop: -1,
+            loopDelay: 300
+        });
+        this.add.text(540, 700, 'Press SPACE to return to title screen',
+        { 
+            fontFamily: 'Times, serif'
+        }).setScale(0.20).setResolution(9);
+    }
+}
---
+++ src/Scenes/Load.js
@@ -0,0 +1,93 @@
+class Load extends Phaser.Scene {
+    constructor() {
+        super("loadScene");
+    }
+
+    preload() {
+
+        //defines a path to your assets folder
+        this.load.setPath("./assets/");
+
+        //load images
+        this.load.image("gem1", "gem_001.png");
+        this.load.image("gem2", "gem_002.png");
+        this.load.image("powerup", "tile_0102.png");
+    
+        // Load audio
+        this.load.audio("playerJump", "phaseJump3.ogg");
+        this.load.audio("playerCollect1", "question_002.ogg");
+        this.load.audio("playerCollect2", "confirmation_004.ogg");
+        this.load.audio("win", "powerUp3.ogg");
+        this.load.audio("error", "error_005.ogg");
+        this.load.audio("unlock", "drop_002.ogg");
+
+        //Load bitmap font
+        this.load.bitmapFont("rocketSquare", "KennyRocketSquare_0.png", "KennyRocketSquare.fnt");
+
+        // Load characters spritesheet
+        this.load.atlas("platformer_characters", "tilemap-characters-packed.png", "tilemap-characters-packed.json");
+
+        // Load tilemap information
+        this.load.image("tilemap_tiles", "monochrome_tilemap_transparent_packed.png");                         // Packed tilemap
+        this.load.tilemapTiledJSON("platformer-level-1", "platformer-level-1.tmj");   // Tilemap in JSON
+        this.load.tilemapTiledJSON("platformer-level-2", "platformer-level-2.tmj");   // Tilemap in JSON
+
+        // Load the tilemap as a spritesheet
+        this.load.spritesheet("tilemap_sheet", "monochrome_tilemap_transparent_packed.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+
+        this.load.multiatlas("kenny-particles", "kenny-particles.json");
+    }
+
+    create() {
+
+        this.anims.create({
+            key: 'gemGlow',
+            defaultTextureKey: "tilemap_sheet",
+            frames: [
+                {key: "gem1"},
+                {key: "gem2"}
+            ],
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNames('platformer_characters', {
+                prefix: "tile_",
+                start: 0,
+                end: 1,
+                suffix: ".png",
+                zeroPad: 4
+            }),
+            frameRate: 15,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'idle',
+            defaultTextureKey: "platformer_characters",
+            frames: [
+                { frame: "tile_0000.png" }
+            ],
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'jump',
+            defaultTextureKey: "platformer_characters",
+            frames: [
+                { frame: "tile_0001.png" }
+            ],
+        });
+
+         // display loading screen
+         this.scene.start("platformerTitleScene");
+    }
+
+    // Never get here since a new scene is started in create()
+    update() {
+    }
+}
